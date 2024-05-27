f9fb5218f2f6e8b4fd0b8317677b76678943e6fa
ZzzihaoGuo <149229033+ZzzihaoGuo@users.noreply.github.com>
2024-05-27 17:25:01 +0800

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,46 @@
+import os
+import random
+
+import torch
+import logging
+import numpy as np
+from torch.distributed import init_process_group, destroy_process_group
+
+def setup_seed(seed):
+
+     torch.manual_seed(seed)
+     torch.cuda.manual_seed_all(seed)
+     np.random.seed(seed)
+     random.seed(seed)
+     torch.backends.cudnn.deterministic = True
+
+
+def set_logger(log_path):
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # 由于每调用一次set_logger函数，就会创建一个handler，会造成重复打印的问题，因此需要判断root logger中是否已有该handler
+    if not any(handler.__class__ == logging.FileHandler for handler in logger.handlers):
+        file_handler = logging.FileHandler(log_path)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)d - %(message)s')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    if not any(handler.__class__ == logging.StreamHandler for handler in logger.handlers):
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
+
+
+def ddp_setup(rank: int, world_size: int):
+   """
+   Args:
+       rank: Unique identifier of each process
+      world_size: Total number of processes
+   """
+   os.environ["MASTER_ADDR"] = "localhost"
+   os.environ["MASTER_PORT"] = "12356"
+   init_process_group(backend="nccl", rank=rank, world_size=world_size)
+   torch.cuda.set_device(rank)
